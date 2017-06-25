93522fe5db074192a5ae259f6cf48de529e88d78
Félix <FelixWolf@users.noreply.github.com>
2017-06-25 06:28:43 -0400

    Added sketti

--- setup.py
+++ setup.py
@@ -3,13 +3,31 @@ from setuptools import setup
 
 setup(
     name='pyverse',
-    version='0.1',
+    version='0.3',
     description='Stub description for pyverse.',
     install_requires=[],
     scripts=[],
     packages=['pyverse'],
-    author='Example Name',
-    author_email='example@example.org',
-    url='http://example.org',
-    download_url='http://example.org/pyverse.tar.gz'
+    author='PyVerse contributors',
+    author_email='pyverse@lists.softhyena.com',
+    maintainer="Félix Wolf",
+    maintainer_email="felix.wolfz@gmail.com",
+    url='https://github.com/FelixWolf/pyverse/',
+    download_url='https://cdn.softhyena.com/pip/pyverse.tar.gz',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
